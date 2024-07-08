a6bb9aeed6e52f1a7ab8cb1c890b2a1e96215cb1
Mykhailo Guivan <tornxid123@gmail.com>
2024-07-08 09:11:38 +0200

    Original code from video tutorial

---
+++ app/src/main/java/com/example/qrappmlkit/QRCodeDecoder.java
@@ -0,0 +1,63 @@
+package com.example.qrappmlkit;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.media.Image;
+
+import androidx.annotation.NonNull;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.common.InputImage;
+
+import java.util.List;
+
+public class QRCodeDecoder implements ImageAnalysis.Analyzer {
+    BarcodeScannerOptions options;
+    BarcodeScanner scanner;
+    Context context;
+
+    public QRCodeDecoder( Context context) {
+        this.context = context;
+        options = new BarcodeScannerOptions.Builder()
+                .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
+                .build();
+        scanner = BarcodeScanning.getClient(options);
+    }
+
+    @Override
+    public void analyze(@NonNull ImageProxy image) {
+        @SuppressLint("UnsafeOptInUsageError")
+        Image mediaImage = image.getImage();
+        if (mediaImage != null){
+            int rotationDeg = image.getImageInfo().getRotationDegrees();
+
+            InputImage iImage = InputImage.fromMediaImage(mediaImage, rotationDeg);
+
+            Task<List<Barcode>> result = scanner.process(iImage);
+            result.addOnSuccessListener(barcode -> {
+                if (barcode.size() > 0) {
+                    Barcode.UrlBookmark urlBookmark = barcode.get(0).getUrl();
+                    String url = null;
+                    try {
+                        url = urlBookmark.getUrl();
+                    } catch (Exception ex) {
+                        url = barcode.get(0).getDisplayValue();
+                    }
+
+                    if (!((MainActivity) context).isProcess && url != null) {
+                        ((MainActivity) context).isProcess = true;
+                        ((MainActivity) context).qrCodeHandler(url);
+                    }
+                }
+                image.close();
+            });
+            result.addOnFailureListener(e -> image.close());
+        }
+    }
+}
